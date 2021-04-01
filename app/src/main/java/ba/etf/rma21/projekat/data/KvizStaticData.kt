2164cc2606d0ce6ac39f567fa7c12a04e05da6c1
mmarincic1 <mmarincic1@etf.unsa.ba>
2021-04-01 02:27:24 +0200

    podeseni datumi da se prikazuju sve 4 boje

--- app/src/main/java/ba/etf/rma21/projekat/data/KvizStaticData.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/KvizStaticData.kt
@@ -7,14 +7,17 @@ fun quizzes(): List<Kviz>{
     // val naziv: String, val nazivPredmeta: String, val datumPocetka: Date, val datumKraj: Date,
     //    val datumRada: Date, val trajanje: Int, val nazivGrupe: String, val osvojeniBodovi: Float
     return listOf(
-        Kviz("Kviz 0", "IM", Date(2021, 4, 20), Date(2021, 4, 20),
-            Date(2021, 4, 20), 30, "G1", 2.5F
+        Kviz("Kviz 0", "IM", Date(2021, 3, 20), Date(2021, 3, 20),
+            null, 30, "G1", null
     ),
         Kviz("Kviz 1", "TP", Date(2021, 4, 22), Date(2021, 4, 22),
-        Date(2021, 4, 22), 3, "G2", 5F
+        null, 3, "G2", null
     ),
-        Kviz("Kviz 2", "RPR", Date(2021, 5, 1), Date(2021, 5, 1),
-        Date(2021, 5, 1), 7, "G3", 1F
+        Kviz("Kviz 2", "RPR", Date(2021, 3, 22), Date(2021, 3, 22),
+        Date(2021, 3, 22), 7, "G3", 1F
+    ),
+        Kviz("Kviz 3", "RMA", Date(2021, 3, 30), Date(2021, 4, 22),
+        null, 7, "G3", null
     )
     );
 }
--- app/src/main/java/ba/etf/rma21/projekat/data/view/KvizListAdapter.kt
+++ app/src/main/java/ba/etf/rma21/projekat/data/view/KvizListAdapter.kt
@@ -11,7 +11,9 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma21.projekat.R
 import ba.etf.rma21.projekat.data.models.Kviz
-import java.time.format.DateTimeFormatter
+import java.time.LocalTime
+import java.util.*
+
 
 class KvizListAdapter(
     private var quizzes: List<Kviz>
@@ -32,15 +34,75 @@ class KvizListAdapter(
     override fun onBindViewHolder(holder: QuizViewHolder, position: Int) {
 
         holder.quizName.text = quizzes[position].naziv
-        holder.quizDate.text = quizzes[position].datumRada.getDate().toString()
-        holder.quizPoints.text = quizzes[position].osvojeniBodovi.toString()
+        if(quizzes[position].datumRada == null){
+            // nije radio kviz
+            if(uporediSaTrenutnimDatumom(quizzes[position].datumPocetka) == 1)
+                holder.quizDate.text = dajDatum(quizzes[position].datumKraj)
+            else holder.quizDate.text = dajDatum(quizzes[position].datumPocetka)
+        }
         holder.quizSubjectName.text = quizzes[position].nazivPredmeta
-        holder.quizTime.text = quizzes[position].trajanje.toString() + "min"
+        holder.quizTime.text = quizzes[position].trajanje.toString() + " min"
         val context: Context = holder.quizStatus.getContext()
+        val status: String = dajStatus(quizzes[position])
+        if(status == "plava") holder.quizPoints.text = quizzes[position].osvojeniBodovi.toString()
+        else holder.quizPoints.text = ""
         var id: Int = context.getResources()
-            .getIdentifier("plava", "drawable", context.getPackageName())
+            .getIdentifier(status, "drawable", context.getPackageName())
         holder.quizStatus.setImageResource(id)
     }
+
+    private fun dajDatum(datumRada: Date): String{
+        var dan: Int = datumRada.getDate()
+        var mjesec: Int = datumRada.getMonth()
+        var danString: String?
+        var mjesecString: String?
+        if(dan < 10){
+            danString = "0" + dan.toString() + "."
+        }
+        else danString = dan.toString() + "."
+        if(mjesec < 10){
+            mjesecString = "0" + mjesec.toString() + "."
+        }
+        else mjesecString = mjesec.toString() + "."
+
+        return danString + mjesecString + datumRada.getYear().toString()
+    }
+
+    private fun uporediSaTrenutnimDatumom(datum1: Date): Int{
+        var godina = Calendar.getInstance().get(Calendar.YEAR)
+        var mjesec = Calendar.getInstance().get(Calendar.MONTH) + 1
+        var dan = Calendar.getInstance().get(Calendar.DATE)
+        if(datum1.getYear() > godina) return 1
+        else if(godina > datum1.getYear()) return 2;
+        else if(datum1.getMonth() > mjesec) return 1;
+        else if(mjesec > datum1.getMonth()) return 2;
+        else if(datum1.getDate() > dan) return 1;
+        else if(dan > datum1.getDate()) return 2;
+        return 0;
+    }
+
+
+    private fun dajStatus(kviz: Kviz): String {
+        // kviz nije uradjen
+        if(kviz.datumRada == null){
+            var datumPocetka = uporediSaTrenutnimDatumom(kviz.datumPocetka)
+            var datumKraja = uporediSaTrenutnimDatumom(kviz.datumKraj)
+            // kviz nije otvoren
+            if(datumPocetka == 1){
+                return "zuta"
+            }
+            // kviz aktivan
+            else if(datumPocetka == 2 && datumKraja == 1){
+                return "zelena"
+            }
+            // kviz nije uradjen i nije aktivan
+            else if(datumPocetka == 2 && datumKraja == 2){
+                return "crvena"
+            }
+        }
+        return "plava"
+    }
+
 
     fun updateMovies(quizzes: List<Kviz>) {
         this.quizzes = quizzes
